9c5375ec806807538dd4dfc21e7382340a939648
Phu <phuktvn0@gmail.com>
2022-11-15 23:17:03 +0700

    update

---
+++ src/components/JobCard.js
@@ -0,0 +1,66 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import Divider from "@mui/material/Divider";
+import {  useLocation, Link } from "react-router-dom";
+import SkillsPaper from "./SkillsPaper";
+import { styled } from "@mui/material/styles";
+
+const CardStyle = styled(Card)(({ theme }) => ({
+  boxShadow: "none",
+  border: "1px solid black",
+  width: "100%",
+  maxWidth: "350px",
+  minWidth: "270px",
+  height: "320px",
+  margin: "auto",
+  backgroundColor: theme.palette.primary.light,
+}));
+
+function JobCard({ description, skills, id, title }) {
+  // const auth = useContext(AuthContext);
+  // const navigate = useNavigate();
+  let location = useLocation();
+  
+  return (
+    <CardStyle ariant="outlined">
+      <Stack
+        direction="column"
+        justifyContent="space-between"
+        alignItems="center"
+        height="100%"
+        padding="5px"
+      >
+        <CardContent>
+          <Typography
+            variant="subtitle1"
+            component="div"
+            sx={{ color: (theme) => theme.palette.common.white }}
+          >
+            {title}
+          </Typography>
+          <Divider />
+          <SkillsPaper skills={skills} />
+          <Typography sx={{ color: (theme) => theme.palette.common.white }}>
+            {description}
+          </Typography>
+        </CardContent>
+        <Button
+          variant="contained"
+          component={Link}
+          to={`/job/${id}`}
+          state={{ backgroundLocation: location }}
+          sx={{ width: "130px" }}
+        >
+          Learn More
+        </Button>
+       
+      </Stack>
+    </CardStyle>
+  );
+}
+
+export default JobCard;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import AuthProvider from "./auth/AuthProvider";
+import ThemeProvider from "./theme";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
